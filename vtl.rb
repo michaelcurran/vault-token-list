92453e0a9b973b4d64c1457581f264c8d7e38be0
michaelcurran <curran.mike@gmail.com>
2016-05-18 15:55:28 -0700

    Update to follow redirects when using vault with HA

--- vtl.rb
+++ vtl.rb
@@ -6,11 +6,35 @@
 require 'net/http'
 require 'json'
 
+$vault_token = ENV['VAULT_TOKEN']
+
 consul_addr = ''
 consul_token = ENV['CONSUL_TOKEN']
 vault_addr = ''
-vault_token = ENV['VAULT_TOKEN']
 
+# Follows redirects when using HA and reaching a standby node
+def fetch(vault_uri, limit = 10)
+  raise ArgumentError, 'too many HTTP redirects' if limit == 0
+
+  req = Net::HTTP::Get.new(vault_uri)
+  req.add_field("X-Vault-Token", "#{$vault_token}")
+
+  resp = Net::HTTP.start(vault_uri.hostname, vault_uri.port) do |http|
+    http.request(req)
+  end
+
+  case resp
+  when Net::HTTPSuccess
+    resp
+  when Net::HTTPRedirection
+    location = resp['location']
+    fetch(URI(location), limit - 1)
+  else
+    puts "There was an error with the connection to #{vault_uri}"
+  end
+end
+
+# Get list of paths to encrypted keys from consul
 consul_uri = URI("#{consul_addr}/v1/kv/vault/sys/token/id?keys")
 consul_req = Net::HTTP::Get.new(consul_uri)
 consul_req.add_field("X-Consul-Token", "#{consul_token}")
@@ -21,17 +45,13 @@ end
 
 body = JSON.parse(consul_resp.body)
 
+# For each path/key in the consul response, query vault to read the key
 body.each do |path|
   # Removes the backend path name from the path, if default this is "vault"
   path.sub!(/^\w+\//, '')
 
   vault_uri = URI("#{vault_addr}/v1/sys/raw/#{path}")
-  vault_req = Net::HTTP::Get.new(vault_uri)
-  vault_req.add_field("X-Vault-Token", "#{vault_token}")
-
-  vault_resp = Net::HTTP.start(vault_uri.hostname, vault_uri.port) do |http|
-    http.request(vault_req)
-  end
+  vault_resp = fetch(vault_uri)
 
   h = JSON.parse(vault_resp.body)
   hv = JSON.parse(h["value"])
